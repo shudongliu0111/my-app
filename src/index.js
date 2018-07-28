5eab7d04c71ef3bfbd77c553468dfc637047c921
unknown <970654226@qq.com>
2018-07-28 21:04:31 +0800

    it can back any step

--- src/index.js
+++ src/index.js
@@ -28,45 +28,49 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  constructor(){
-    super();
-    this.state ={
-      squares: Array(9).fill(null),
-      xIsNext: true, //判断下一个落子方，当前是X
-    }
-  }
+  // constructor(){
+  //   super();
+  //   this.state ={
+  //     squares: Array(9).fill(null),
+  //     xIsNext: true, //判断下一个落子方，当前是X
+  //   }
+  // } 
+  // 状态提升到game 所以和square一样 删掉构造方法
   renderSquare(i) {
     return (
       <Square 
-        value={this.state.squares[i]} 
-        onClick = {()=>this.handleClick(i)}
+        value={this.props.squares[i]} 
+        onClick = {()=>this.props.onClick(i)}
       />
     );
   }
   //处理点击事件函数 修改组件状态 并且实现交替落子
-  handleClick(i){
-    // alert(this.state.xIsNext);
-    const squares = this.state.squares.slice();
-    // 判断落子方 修改状态
-    squares[i] = this.state.xIsNext? 'X' : 'O';
-    this.setState({
-      squares, //squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
+  // handleClick(i){
+  //   const squares = this.state.squares.slice();
+  //   // alert('winner：'+ calculateWinner(squares) + '有没有：'+squares[i]);
+  //   if (calculateWinner(squares) || squares[i]) {
+  //     // 没有赢家返回 null 该位置没有值返回的是 默认值 null
+  //     return;
+  //   }
+  //   // 判断落子方 修改状态
+  //   squares[i] = this.state.xIsNext? 'X' : 'O';
+  //   this.setState({
+  //     squares, //squares: squares,
+  //     xIsNext: !this.state.xIsNext,
+  //   });
+  // }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
+    // const winner = calculateWinner(this.state.squares);
+    // let status;
+    // if (winner) {
+    //   status = 'Winner: ' + winner;
+    // } else {
+    //   status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    // }
 
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -88,15 +92,76 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      history:[
+        {
+          squares:Array(9).fill(null),
+        }
+      ],
+      xIsNext:true,
+      stepNumber:0
+    };
+  }
+
+ handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares
+      }]),
+      xIsNext: !this.state.xIsNext,
+      stepNumber :history.length
+    });
+  }
+
+  jumpTo(step){
+    this.setState({
+      stepNumber:step,
+      xIsNext: (step % 2)? false : true
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step,move)=> {
+      const desc = move? "返回到"+ move : "开始游戏";
+      return  (
+        <li key={move}>
+          <button onClick={()=>this.jumpTo(move)}>{desc}</button>
+        </li>
+      )
+    })
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+           <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{ status }</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
